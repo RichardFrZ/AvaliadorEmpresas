19d3e846a92af1b87589cc9db371b7e63b8d3b04
Richard <ri1452144@gmail.com>
2024-07-12 12:23:38 -0300

    Functions getQuestion + getAllQuestions added

--- app/src/main/java/com/example/avaliadordeempresas/database/DBHelper.kt
+++ app/src/main/java/com/example/avaliadordeempresas/database/DBHelper.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.avaliadordeempresas.model.QuestionModel
 
 class DBHelper(context: Context) : SQLiteOpenHelper(context, "perguntas.db", null, 1) {
 
@@ -16,7 +17,7 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, "perguntas.db", nul
         "INSERT INTO questions (question) VALUES ('Empresa apresentou crescimento de receita nos últimos 5 anos')",
         "INSERT INTO questions (question) VALUES ('Empresa possui ROE acima de 10%')",
         "INSERT INTO questions (question) VALUES ('Margem de lucro acima de 15%?')",
-        "INSERT INTO questions (question) VALUES ('Empresa possui liquidez diária acima de R$ 2M NAS ON'S')",
+        "INSERT INTO questions (question) VALUES ('Empresa possui liquidez diária acima de R$ 2M NAS ONS')",
         "INSERT INTO questions (question) VALUES ('Empresa pagou +5% de dividendos/ano nos últimos 5 anos')",
         "INSERT INTO questions (question) VALUES ('Empresa possui dívida menor que patrimônio')"
     )
@@ -54,5 +55,49 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, "perguntas.db", nul
         val res = db.delete("questions", "id=?", arrayOf(id.toString()))
         db.close()
         return res
+    }
+
+    fun getQuestion(id: Int): QuestionModel {
+        val db = this.readableDatabase
+        val c = db.rawQuery(
+            "SELECT * FROM questions WHERE id=?",
+            arrayOf(id.toString())
+        )
+        var questionModel = QuestionModel()
+
+        if (c.count == 1) {
+            c.moveToFirst()
+            val idIndex = c.getColumnIndex("id")
+            val questionIndex = c.getColumnIndex("question")
+            questionModel = QuestionModel(
+                id = c.getInt(idIndex),
+                textQuestion = c.getString(questionIndex)
+            )
+        }
+        db.close()
+        return questionModel
+    }
+
+    fun getAllQuestions(): ArrayList<QuestionModel> {
+        val db = this.readableDatabase
+        val c = db.rawQuery(
+            "SELECT * FROM questions", null
+        )
+        val listQuestionModel = ArrayList<QuestionModel>()
+
+        if (c.count > 0) {
+            c.moveToFirst()
+            val idIndex = c.getColumnIndex("id")
+            val questionIndex = c.getColumnIndex("question")
+            do {
+                val questionModel = QuestionModel(
+                    id = c.getInt(idIndex),
+                    textQuestion = c.getString(questionIndex)
+                )
+                listQuestionModel.add(questionModel)
+            } while (c.moveToNext())
+        }
+        db.close()
+        return listQuestionModel
     }
 }
